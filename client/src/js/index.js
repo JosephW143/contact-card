29d273351913f3b830b3fd5ce8f342699d430674
JosephW143 <josephwright1436@gmail.com>
2022-10-16 14:48:02 -0400

    delete

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -16,7 +16,7 @@ import Dog from '../images/dog.png';
 
 import { initdb } from './database';
 
-import { initDb, getDb, postDb } from './database';
+import { initDb, getDb, postDb, deleteDb } from './database';
 
 window.addEventListener('load', function() {
   initdb();
@@ -25,3 +25,12 @@ window.addEventListener('load', function() {
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
 });
+
+window.deleteCard = (e) => {
+  // Grabs the id from the button element attached to the contact card.
+  let id = parseInt(e.id);
+  // Delete the card
+  deleteDb(id);
+  // Reload the DOM
+  fetchCards();
+};
